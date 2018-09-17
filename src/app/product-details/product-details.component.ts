047859d388b1599958f103f847f9833d0942f920
rkucherenko <rkucherenko@10-4.com>
2018-09-18 00:14:05 +0300

    added face companyList for dropdown

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import Product from '../interfaces/product';
+import Company from '../interfaces/company';
 
 @Component({
   selector: 'app-product-details',
@@ -7,11 +8,13 @@ import Product from '../interfaces/product';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
+  companies: Company[];
   @Input()
   product: Product;
 
-  constructor() { }
+  constructor() {
+    this.companies = [{id: 1, name: 'qwe'}, {id: 2, name: 'asd'}];
+  }
 
   ngOnInit() {
   }
